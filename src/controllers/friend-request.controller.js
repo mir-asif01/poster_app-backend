ad93904e93bbd209c671fc30bee58e16a3457d27
mir-asif01 <asifbsb2002@gmail.com>
2024-06-28 00:45:39 +0600

    request sending route tested successfully

--- src/controllers/friend-request.controller.js
+++ src/controllers/friend-request.controller.js
@@ -1,25 +1,32 @@
-import { FriendRequest } from "../models/friend-request.model"
+import { FriendRequest } from "../models/friend-request.model.js"
 
+// const requsetModel = {
+//     senderId,
+//     recieverId,
+//     status
+// }
 const sendFriendRequest = async (req, res) => {
-    // const requsetModel = {
-    //     senderId,
-    //     recieverId,
-    //     status
-    // }
 
-    const { senderId, recieverId } = req.body
-    const friendRequest = {
-        senderId,
-        recieverId
+    try {
+        const { senderId, recieverId } = req.body
+        const friendRequest = {
+            senderId,
+            recieverId
+        }
+        console.log(friendRequest);
+        if (!senderId) {
+            res.send({ success: false, message: "sender Id is missing" })
+        }
+        if (!recieverId) {
+            res.send({ success: false, message: "reciever Id is missing" })
+        }
+        else {
+            const result = await FriendRequest.create(friendRequest)
+            res.send({ success: true, message: "Request sent!" })
+        }
+    } catch (error) {
+        if (error) console.log(error)
     }
-    if (!senderId) {
-        res.send({ success: false, message: "sender Id is missing" })
-    }
-    if (!recieverId) {
-        res.send({ success: false, message: "reciever Id is missing" })
-    }
-    const result = await FriendRequest.create(friendRequest)
-    res.send({ success: true, message: "Request sent!" }, result)
 }
 
 
