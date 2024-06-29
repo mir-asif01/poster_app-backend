6071414c3c00e1d2711a2c25ed7636ffa61e10ce
mir-asif01 <asifbsb2002@gmail.com>
2024-06-30 01:25:12 +0600

    post controller structure added

---
+++ src/controllers/post.controller.js
@@ -0,0 +1,33 @@
+const createPost = async (req,res)=>{
+    try {
+        
+    } catch (error) {
+        if(error) console.log(error)
+    }
+}
+
+const getPostsForPostsPage = async (req,res) =>{
+    try {
+        
+    } catch (error) {
+        if(error) console.log(error);
+    }
+}
+
+const getPostsAddedByUser = async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        if(error) console.log(error);
+    }
+}
+
+const deletePost = async(req,res) =>{
+    try {
+        
+    } catch (error) {
+        if(error) console.log(error);
+    }
+}
+
+export {createPost,deletePost,getPostsForPostsPage,getPostsAddedByUser}
