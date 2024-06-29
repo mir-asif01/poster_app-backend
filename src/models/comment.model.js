69846d6f694816c67186806b2eb914b9418a745b
mir-asif01 <asifbsb2002@gmail.com>
2024-06-30 01:20:31 +0600

    comment model created

---
+++ src/models/comment.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+    {
+        postId : {
+            type : String,
+            required : true
+        },
+        comment : {
+            type : String,
+            required : true
+        }
+    },
+    {
+        timestamps : true
+    }
+)
+
+export const Comment = mongoose.model("Comment",commentSchema)
