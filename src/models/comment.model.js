4573e2b91c1a701581c12c2bcadd7ce84a9fbfa3
mir-asif01 <asifbsb2002@gmail.com>
2024-07-01 23:20:05 +0600

    get all comments controller added

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -16,4 +16,13 @@ const commentSchema = new mongoose.Schema(
     }
 )
 
+const getAllComments = async (req,res) => {
+    try {
+        const comments = await Comment.find()
+        res.send({success:true,message: "All comments sent!!" })
+    } catch (error) {
+        if(error) console.log(error);
+    }
+}
+
 export const Comment = mongoose.model("Comment",commentSchema)
