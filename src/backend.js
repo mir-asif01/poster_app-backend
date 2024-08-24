d5cbf08b766aad621eacacf67ef07aad948f6e50
mir-asif01 <asifbsb2002@gmail.com>
2024-08-24 08:31:52 +0600

    removed jwt middleware from routes

--- src/backend.js
+++ src/backend.js
@@ -1,135 +1,102 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-dotenv.config();
-app.use(cors());
-app.use(express.json());
+dotenv.config()
+app.use(cors())
+app.use(express.json())
 
 //controllers import
 import {
-  registerUser,
-  loginUser,
-  getAllUsers,
-} from "./controllers/user.controller.js";
-import { upload } from "./utils/multer.util.js";
-import verifyJWT from "./middlewares/jwt.middleware.js";
+   registerUser,
+   loginUser,
+   getAllUsers,
+} from "./controllers/user.controller.js"
+import { upload } from "./utils/multer.util.js"
+import verifyJWT from "./middlewares/jwt.middleware.js"
 import {
-  acceptRequest,
-  cancelRequest,
-  getAllRequestsList,
-  sendFriendRequest,
-} from "./controllers/friend-request.controller.js";
+   acceptRequest,
+   cancelRequest,
+   getAllRequestsList,
+   sendFriendRequest,
+} from "./controllers/friend-request.controller.js"
 import {
-  addOneLike,
-  createPost,
-  getPostsAddedByUser,
-  getPostsForPostsPage,
-  getSinglePost,
-} from "./controllers/post.controller.js";
+   addOneLike,
+   createPost,
+   getPostsAddedByUser,
+   getPostsForPostsPage,
+   getSinglePost,
+} from "./controllers/post.controller.js"
 import {
-  addComment,
-  getAllCommentUserAdded,
-  getAllComments,
-} from "./controllers/comment.controller.js";
+   addComment,
+   getAllCommentUserAdded,
+   getAllComments,
+} from "./controllers/comment.controller.js"
 
 app.get("/", (req, res) => {
-  res.send("backend server running!!");
-});
-
-// const dummyUser = {
-//     userName: "asif17",
-//     email: "asif@gmai.com",
-//     password: "##########",
-//     fullName: "Mir Kamrul Ahsan Asif",
-//     profileImage: "",
-//     coverImage: "",
-//     about: "i am a developer from bangladesh. currently looking for a intern or full time oppurtunity! thank you",
-//     currentPosition: "N/A",
-//     educationalInformations: [
-//         {
-//             degreeName: "Diploma in Computer",
-//             passingYear: "2024"
-//         },
-//         {
-//             degreeName: "SSC",
-//             passingYear: "2020"
-//         }
-//     ],
-//     technicalSkills: [
-//         {
-//             skill: "C",
-//             experience: "3",
-//         },
-//         {
-//             skill: "JavaScript",
-//             experience: "3",
-//         },
-//         {
-//             skill: "ReactJS",
-//             experience: "2.5",
-//         },
-//     ],
-//     facebookProfileLink: "",
-//     githubProfileLink: "",
-//     linkedInProfileLink: "",
-// }
+   res.send("backend server running!!")
+})
 
 async function main() {
-  const URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mtnbd39.mongodb.net/poster_app?retryWrites=true&w=majority&appName=Cluster0`;
-  try {
-    await mongoose
-      .connect(URI)
-      .then((res) => console.log("db connected"))
-      .catch((err) => console.log("ERROR : ", err));
+   const URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mtnbd39.mongodb.net/poster_app?retryWrites=true&w=majority&appName=Cluster0`
+   try {
+      await mongoose
+         .connect(URI)
+         .then((res) => console.log("db connected"))
+         .catch((err) => console.log("ERROR : ", err))
 
-    // user authentication and authorizaton related api endpoints here
-    app.post(
-      "/register",
-      upload.fields([
-        {
-          name: "profileImage",
-          maxCount: 1,
-        },
-        {
-          name: "coverImage",
-          maxCount: 1,
-        },
-      ]),
-      registerUser
-    );
-    app.post("/login", loginUser);
-    app.get("/users", verifyJWT, getAllUsers);
+      // user authentication and authorizaton related api endpoints here
+      app.post(
+         "/register",
+         upload.fields([
+            {
+               name: "profileImage",
+               maxCount: 1,
+            },
+            {
+               name: "coverImage",
+               maxCount: 1,
+            },
+         ]),
+         registerUser
+      )
+      app.post("/login", loginUser)
+      app.get("/users", getAllUsers)
 
-    // friend request add/cancel api endpoints
-    app.post("/add-friend", verifyJWT, sendFriendRequest);
-    app.post("/accept-request", verifyJWT, acceptRequest);
-    app.post("/cancel-request", verifyJWT, cancelRequest);
+      // friend request add/cancel api endpoints
+      app.post("/add-friend", sendFriendRequest)
+      app.post("/accept-request", acceptRequest)
+      app.post("/cancel-request", cancelRequest)
 
-    // fetch all requests for friend request page
-    app.get("/all-request/:id", verifyJWT, getAllRequestsList);
+      // fetch all requests for friend request page
+      app.get("/all-request/:id", getAllRequestsList)
 
-    // post related routes
-    app.post("/create-post", verifyJWT, upload.single("postImage"), createPost);
-    app.get("/posts/:id", verifyJWT, getSinglePost);
-    app.post("/add-one-like", verifyJWT, addOneLike);
-    app.get("/posts-by-user", verifyJWT, getPostsAddedByUser);
-    app.get("/posts", verifyJWT, getPostsForPostsPage);
+      // post related routes
+      app.post(
+         "/create-post",
+         verifyJWT,
+         upload.single("postImage"),
+         createPost
+      )
+      app.get("/posts/:id", getSinglePost)
+      app.post("/add-one-like", addOneLike)
+      app.get("/posts-by-user", getPostsAddedByUser)
+      app.get("/posts", getPostsForPostsPage)
 
-    // comment adding api
-    app.post("/add-comment", verifyJWT, addComment);
-    app.get("/all-comments", verifyJWT, getAllComments);
-    app.get("/all-user-comments", verifyJWT, getAllCommentUserAdded);
-  } catch (error) {
-    if (error) console.log(error);
-  }
+      // comment adding api
+      app.post("/add-comment", addComment)
+      app.get("/all-comments", getAllComments)
+      app.get("/all-user-comments", getAllCommentUserAdded)
+   } catch (error) {
+      if (error) console.log(error)
+   }
 }
-main().catch((err) => console.log(err));
+main().catch((err) => console.log(err))
 
 app.listen(port, () => {
-  console.log(`App is running on http://localhost:3000`);
-});
+   console.log(`App is running on http://localhost:3000`)
+})
